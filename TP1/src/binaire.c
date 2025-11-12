70b4716fb0d6acc2e8ac8ad22ab2f18af211ad87
nourbek21 <nourbek31@icloud.com>
2025-11-12 15:19:43 +0100

    Update binaire.c

--- TP1/src/binaire.c
+++ TP1/src/binaire.c
@@ -1,33 +1,26 @@
 #include <stdio.h>
 
-int main() {
-    int nombres[] = {0, 4096, 65536, 65535, 1024};
+int main(void)
+{
+    unsigned int nombres[] = {0, 4096, 65536, 65535, 1024};
     int taille = sizeof(nombres) / sizeof(nombres[0]);
 
-    printf("=== Affichage en binaire ===\n\n");
-
     for (int n = 0; n < taille; n++) {
-        int nombre = nombres[n];
-        printf("Nombre : %d\nBinaire : ", nombre);
+        unsigned int nombre = nombres[n];
+        printf("%u en binaire : ", nombre);
 
-        // On parcourt les bits du plus significatif au moins significatif (32 bits)
-        int nb_bits = sizeof(int) * 8;
-        int bitTrouve = 0; // pour éviter d'afficher les zéros inutiles au début
+        int bits = sizeof(nombre) * 8; // généralement 32 bits
+        int affichage_commence = 0;    // pour ignorer les zéros initiaux
 
-        for (int i = nb_bits - 1; i >= 0; i--) {
+        for (int i = bits - 1; i >= 0; i--) {
             int bit = (nombre >> i) & 1;
-            if (bit)
-                bitTrouve = 1;
-            if (bitTrouve)
+            if (bit == 1) affichage_commence = 1; // on commence dès le premier 1
+            if (affichage_commence || i == 0)
                 printf("%d", bit);
         }
-
-        // Si le nombre est 0, on affiche simplement 0
-        if (!bitTrouve)
-            printf("0");
-
-        printf("\n\n");
+        printf("\n");
     }
 
     return 0;
 }
+
