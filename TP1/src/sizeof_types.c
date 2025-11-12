84752adbfac11bf9ff9436b0f00d47ea5cf68399
nourbek21 <nourbek31@icloud.com>
2025-11-12 14:40:22 +0100

    Update sizeof_types.c

---
+++ TP1/src/sizeof_types.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    printf("Tailles des types de base en octets :\n\n");
+
+    printf("char : %zu octets\n", sizeof(char));
+    printf("signed char : %zu octets\n", sizeof(signed char));
+    printf("unsigned char : %zu octets\n", sizeof(unsigned char));
+
+    printf("short : %zu octets\n", sizeof(short));
+    printf("unsigned short : %zu octets\n", sizeof(unsigned short));
+
+    printf("int : %zu octets\n", sizeof(int));
+    printf("unsigned int : %zu octets\n", sizeof(unsigned int));
+
+    printf("long int : %zu octets\n", sizeof(long int));
+    printf("unsigned long int : %zu octets\n", sizeof(unsigned long int));
+
+    printf("long long int : %zu octets\n", sizeof(long long int));
+    printf("unsigned long long int : %zu octets\n", sizeof(unsigned long long int));
+
+    printf("float : %zu octets\n", sizeof(float));
+    printf("double : %zu octets\n", sizeof(double));
+    printf("long double : %zu octets\n", sizeof(long double));
+
+    return 0;
+}
