d161dff04ea74ac6aa22ef58ffe3cc356e993d52
nourbek21 <nourbek31@icloud.com>
2025-11-19 14:41:17 +0100

    Update sizeof.c

---
+++ TP3/src/sizeof.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    printf("La taille de int est       : %zu octets\n", sizeof(int));
+    printf("La taille de int* est      : %zu octets\n", sizeof(int*));
+    printf("La taille de int** est     : %zu octets\n", sizeof(int**));
+
+    printf("La taille de char est      : %zu octets\n", sizeof(char));
+    printf("La taille de char* est     : %zu octets\n", sizeof(char*));
+    printf("La taille de char** est    : %zu octets\n", sizeof(char**));
+    printf("La taille de char*** est   : %zu octets\n", sizeof(char***));
+
+    printf("La taille de float est     : %zu octets\n", sizeof(float));
+    printf("La taille de float* est    : %zu octets\n", sizeof(float*));
+    printf("La taille de float** est   : %zu octets\n", sizeof(float**));
+    printf("La taille de float*** est  : %zu octets\n", sizeof(float***));
+
+    printf("La taille de double est    : %zu octets\n", sizeof(double));
+    printf("La taille de double* est   : %zu octets\n", sizeof(double*));
+
+    printf("La taille de long int est  : %zu octets\n", sizeof(long int));
+    printf("La taille de long* est     : %zu octets\n", sizeof(long*));
+
+    printf("La taille de long long est : %zu octets\n", sizeof(long long));
+    printf("La taille de long long* est: %zu octets\n", sizeof(long long*));
+
+    printf("La taille de void* est     : %zu octets\n", sizeof(void*));
+
+    return 0;
+}
